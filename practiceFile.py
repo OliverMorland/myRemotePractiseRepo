c003a40d3317e96033e06bb5f69a047c2984a3aa
Oliver Morland <oliversimonedward@hotmail.co.uk>
2020-01-29 10:42:54 -0500

    First Commit

---
+++ practiceFile.py
@@ -0,0 +1,12 @@
+#This program says hello and asks for my name.
+
+print('Hello World')
+
+#Ask for name
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of my name is:')
+print(len(myName))
+
+ 
