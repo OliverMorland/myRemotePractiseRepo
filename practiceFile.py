05d245e7c55cb443f0401d513a88a1e57dcd2ab2
Oliver Morland <oliversimonedward@hotmail.co.uk>
2020-01-29 11:32:05 -0500

    Text improvements made.

--- practiceFile.py
+++ practiceFile.py
@@ -1,11 +1,11 @@
 #This program says hello and asks for my name.
 
-print('Hello World')
+print('Hello Stranger')
 
 #Ask for name
 print('What is your name?')
 myName = input()
-print('It is good to meet you, ' + myName)
+print('Its a pleasure to meet you, ' + myName)
 print('The length of my name is:')
 print(len(myName))
 
